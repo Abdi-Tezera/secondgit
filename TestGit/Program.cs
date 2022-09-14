a3cde6c6e87a4b4243e68da21985a352dc840057
Abdi-Tezera <112860935+Abdi-Tezera@users.noreply.github.com>
2022-09-14 12:22:30 +0200

    code change with loop

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,16 +1,20 @@
 ﻿Console.WriteLine("Välj ett användarnamn och tryck enter> ");
 String userName = Console.ReadLine();bool checkNamn = true;
+String[] storedNames = new string[] { "Abdi", "Nathan", "Erik" };
 while (checkNamn)
 {
-    if (userName == "Abdi")
+    for (int i = 0; i < storedNames.Length; i++)
     {
-        Console.WriteLine("-Fel: Användarnamnet är upptaget. Försök igen.");
-        Console.WriteLine("Ange användarnamn på nytt.");
-        userName = Console.ReadLine();
-    }
-    else
-    {
-    Console.WriteLine("Grattis du har loggat in");
-        checkNamn = false;
+        if (userName == storedNames[i])
+        {
+            Console.WriteLine("-Fel: Användarnamnet är upptaget. Försök igen.");
+            Console.WriteLine("Ange användarnamn på nytt.");
+            userName = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Grattis du har loggat in");
+            checkNamn = false;break;
+        }
     }
 }
