f37c00524e2763f0087357c77b05edd647b0a6d9
Abdi-Tezera <112860935+Abdi-Tezera@users.noreply.github.com>
2022-09-14 11:59:37 +0200

    First change

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,1 +1,16 @@
 ﻿Console.WriteLine("Välj ett användarnamn och tryck enter> ");
+String userName = Console.ReadLine();bool checkNamn = true;
+while (checkNamn)
+{
+    if (userName == "Abdi")
+    {
+        Console.WriteLine("-Fel: Användarnamnet är upptaget. Försök igen.");
+        Console.WriteLine("Ange användarnamn på nytt.");
+        userName = Console.ReadLine();
+    }
+    else
+    {
+    Console.WriteLine("Grattis du har loggat in");
+        checkNamn = false;
+    }
+}
